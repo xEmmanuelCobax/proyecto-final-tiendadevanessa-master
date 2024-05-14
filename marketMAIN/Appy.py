32d46bb4febf39f289337647ea24e36daa806d6d
xEmmanuelCobax <emmanuelcobacuevas@gmail.com>
2024-05-14 02:42:16 -0600

    v12

--- marketMAIN/Appy.py
+++ marketMAIN/Appy.py
@@ -20,33 +20,32 @@ sc = Blueprint("shortcut", __name__, url_prefix="/shortcut")
 
 app.config["SECRET_KEY"] = config.HEX_SEC_KEY
 
+
 @auth.route("/signin", methods=["GET", "POST"])
 def signin():
     print("========================================\nRUTA-Signing\n")
     if "email" in session:
         print("========================================\nEmail In Session\n")
-        return render_template("profile.weclomeuser", email=session["email"])
+        return render_template("profile/welcome-user.html", email=session["email"])
     elif request.method == "POST":
         email = request.form["email"]
         password = request.form["password"]
         print("========================================\nDATOS OBTENIDOS>\n")
-        print("Email - {}".format(email))
-        print("Password - {}".format(password))
+        print(f"Email - {email}")
+        print(f"Password - {password}")
         print("========================================")
         existing_email = config.Read(
-            "SELECT * FROM dbo.Usuarios WHERE CORREO = '{0}'".format(email)
+            "SELECT * FROM dbo.Usuarios WHERE CORREO = '{}'".format(email)
         )
 
         if not existing_email:
             # El correo electrónico no está registrado
             email_not_found = True
             print(
-                "========================================\ncorreo electrónico no está registrado\n========================================"
+                "========================================\nCorreo electrónico no está registrado\n========================================"
             )
-            return render_template(
-                "auth/signin.html", email_not_found=email_not_found
-            )
-        
+            return render_template("auth/signin.html", email_not_found=email_not_found)
+
         else:
             # El correo electrónico está registrado
             if existing_email[0][6] == password:
@@ -56,7 +55,6 @@ def signin():
                     "========================================\nContraseña correcta\n========================================"
                 )
                 return redirect(url_for("profile.welcomeuser", user=email))
-            
             else:
                 # Contraseña incorrecta
                 bad_password = True
@@ -70,10 +68,10 @@ def signin():
     return render_template("auth/signin.html")
 
 
-@auth.route("/signup", methods=["GET", "POST"])  # Altas
+@auth.route("/signup", methods=["GET", "POST"])
 def signup():
     print("========================================\nRUTA-Signup\n")
-    # Verificar si hay una dirreccion de correo dentro de session
+    # Verificar si hay una dirección de correo dentro de la sesión
     if "email" in session:
         return render_template("index.html", email=session["email"])
     elif request.method == "POST":
@@ -87,10 +85,10 @@ def signup():
         password = request.form.get("password")
 
         print("========================================\nDATOS OBTENIDOS>\n")
-        print("Name - {}".format(name))
-        print("Lastname - {}".format(lastname))
-        print("Email - {}".format(email))
-        print("Password - {}".format(password))
+        print(f"Name - {name}")
+        print(f"Lastname - {lastname}")
+        print(f"Email - {email}")
+        print(f"Password - {password}")
         print("========================================")
 
         aux = lastname.split()
@@ -101,11 +99,13 @@ def signup():
             lastname_error = True
 
         existing_email = config.Read(
-            "SELECT * FROM dbo.Usuarios WHERE CORREO = '{0}'".format(email)
+            "SELECT * FROM dbo.Usuarios WHERE CORREO = '{}'".format(email)
         )
 
         existing_user = config.Read(
-            "SELECT * FROM dbo.Usuarios WHERE NOMBRE = '{0}' AND AP_PAT = '{1}' AND AP_MAT = '{2}'".format(name, apellido_paterno, apellido_materno)
+            "SELECT * FROM dbo.Usuarios WHERE NOMBRE = '{}' AND AP_PAT = '{}' AND AP_MAT = '{}'".format(
+                name, apellido_paterno, apellido_materno
+            )
         )
 
         # Existe el correo en la base de datos
@@ -122,51 +122,52 @@ def signup():
                 lastname_error=lastname_error,
             )
 
-        # No hay ningun error
+        # No hay ningún error
         else:
-            # registrar en base de datos
+            # Registrar en base de datos
             config.CUD(
-                "INSERT INTO dbo.Usuarios (NOMBRE, AP_PAT , AP_MAT , CORREO , CONTRASENA, ESTATUS) VALUES ('{0}', '{1}', '{2}', '{3}', '{4}' , 1)".format(
+                "INSERT INTO dbo.Usuarios (NOMBRE, AP_PAT , AP_MAT , CORREO , CONTRASENA, ESTATUS) VALUES ('{}', '{}', '{}', '{}', '{}' , 1)".format(
                     name, apellido_paterno, apellido_materno, email, password
                 )
             )
-            print("Renderizar auth/sigin")
-            return url_for("auth.signin.html", registration_successful=True)
+            print("Renderizar auth/signin")
+            return redirect(url_for("auth.signin", registration_successful=True))
     else:
         return render_template("auth/signup.html")
 
 
-# Define tu vista para el registro exitoso aquí
 @app.route("/successful_registration")
 def successful_registration():
     return render_template("auth/successful-registration.html")
+
 
 @products.route("/create_product", methods=["POST"])
 def create_product():
     if "email" in session:
         if request.method == "POST":
+            # En la tabla Almacen
             nombre = request.form.get("nombre")
             cantidad = request.form.get("cantidad")
-            compania = request.form.get("compania")
             precio = request.form.get("precio")
+            # En la tabla compania EXISTENTE, obtener el id de la compa
+            compania = request.form.get("compania")
+            # En la tabla intermediario EXISTENTE obtener el id interm
+            compania = request.form.get("intermediario")
+            
             config.CUD(
-                "INSERT INTO tasks (nombre, cantidad, descripcion, email, fecha) VALUES (%s, %s, %s, %s, %s)", (nombre, cantidad , compalia,session['email'])
+                "INSERT INTO tasks (nombre, cantidad, descripcion, email, fecha) VALUES (%s, %s, %s, %s, %s)",
+                (nombre, cantidad, compania, precio, session["email"]),
             )
             return redirect(url_for("products.warehouse"))
     else:
         return redirect(url_for("auth.signin"))
 
+
 @products.route("/warehouse")
 def warehouse():
-    # solamente es la vista general de los productos, capaz tenga que actualizarla (re plantearla)
-    # no veo necesario hacer algo por el momento más que muestre todos los productos
     if "email" in session:
-        # wewewewewewe aqui de devuelvo una matriz?
-        # yep, solo devuelve una matriz con todos los datos hasta el momento.
-        # tecnicamente en todas devuelves una matriz, en la de companias si creo que debes hacer un producto carteasiano para mostrarlo todo
-        # Correcto, checar en pagina
         products = config.Read(
-            "select dbo.Almacen.ID_PRODUCTO,dbo.Almacen.NOMBRE,dbo.Almacen.PRECIO_UNITARIO, dbo.Almacen.EXISTENCIAS From dbo.Almacen"
+            "SELECT ID_PRODUCTO, NOMBRE, PRECIO_UNITARIO, EXISTENCIAS FROM dbo.Almacen WHERE dbo.Almacen.ESTATUS = 1"
         )
         return render_template("products/warehouse.html", products=products)
     else:
@@ -175,62 +176,105 @@ def warehouse():
 
 @products.route("/delete_product", methods=["POST"])
 def delete_product():
+    # Verificar si el usuario tiene una sesión activa
+    if "email" in session:
+        # Verificar si el método de solicitud es POST
+        if request.method == "POST":
+            # Obtener el ID del producto de los datos del formulario
+            product_id = request.form.get("product_id")
+            print("========================================\nDATOS OBTENIDOS>\n")
+            print(f"Product_ID - {product_id}")
+            print("========================================")
+            config.CUD(
+                "UPDATE dbo.Almacen SET ESTATUS = 0 WHERE ID_PRODUCTO = {}".format(product_id)
+            )
+            return redirect(url_for("products.managewarehouse"))
+    # Si el usuario no tiene una sesión activa, redirigirlo a la página de inicio de sesión
+    return redirect(url_for("auth.signin"))
+
+@products.route("/search_product", methods=["GET", "POST"])
+def search_product():
     if "email" in session:
         if request.method == "POST":
-            product_id = request.form.get("product_id")
-            print("Producto eliminado con ID: {}".format(product_id))
-            query_delete = "DELETE FROM dbo.Almacen WHERE ID_PRODUCTO = {}".format(product_id)
-            config.CUD(query_delete)
-            return redirect(url_for("products.warehouse"))
+            search_term = request.form.get("search_term")
+            products = config.Read(
+                "SELECT ID_PRODUCTO, NOMBRE, PRECIO_UNITARIO, EXISTENCIAS FROM dbo.Almacen WHERE NOMBRE LIKE '%{}%' AND dbo.Almacen.ESTATUS = 1".format(
+                    search_term
+                )
+            )
+            return render_template("products/warehouse.html", products=products)
     else:
         return redirect(url_for("auth.signin"))
 
 
-@products.route("/manage_warehouse")
+@products.route("/manage_warehouse", methods=["GET", "POST"])
 def managewarehouse():
-    # maneja todos los comproductos del almacen ya sea agregar nuevos productos, eliminarnos, editarlos
-    # actualmente creo que el boton de eliminar no funciona (no hice referencia a que capture los datos de la fila en la que esta)
-    # al boton de eliminar le ocurre lo mismo.
-    # cuando ajusten el html para que se ingresen los datos en la base de datos y lo muestre la tabla, me dicen para empezar a ver que pedo con esa parte.
     if "email" in session:
         products = config.Read(
-            "select dbo.Almacen.ID_PRODUCTO,dbo.Almacen.NOMBRE,dbo.Almacen.PRECIO_UNITARIO, dbo.Almacen.EXISTENCIAS From dbo.Almacen"
+            "SELECT ID_PRODUCTO, NOMBRE, PRECIO_UNITARIO, EXISTENCIAS FROM dbo.Almacen WHERE dbo.Almacen.ESTATUS = 1"
         )
-        company = config.Read("select dbo.Proveedor.NOMBRE From dbo.Proveedor")
+        members = config.Read(
+            "SELECT Proveedor.ID_COMPANIA, ID_INTERMEDIARIO ,Proveedor.NOMBRE, Intermediario.NOMBRE, Intermediario.AP_PAT, Intermediario.AP_MAT FROM dbo.Proveedor INNER JOIN dbo.Intermediario ON Intermediario.ID_COMPANIA = Proveedor.ID_COMPANIA WHERE dbo.Proveedor.ESTATUS = 1 AND dbo.Intermediario.ESTATUS = 1"
+        )
         if request.method == "POST":
             print("hola")
         return render_template(
-            "products/Manage-warehouse.html", products=products, company=company
+            "products/manage-warehouse.html",
+            products=products,
+            members=members,
         )
-        # Agregar productos
     else:
-        return redirect(url_for("signin"))
+        return redirect(url_for("auth.signin"))
 
 
 @products.route("/manage_presentation")
-# maneja la presenracion pues como te planteee una tabla de un unico campo (dos con el id)
-# puede agregar, editar e eliminar presentaciones
 def managepresentation():
     return render_template("products/manage-presentation.html")
 
+
 @products.route("/manage_company")
 def managecompany():
-    # al igual que las anteriores busca manejar tanto las companias como los intermediados,
-    # asocie la vista para que se vea la relacion entre la compañia y el intermediado
-    # puede eliminar compania e intermediado actual, editar y crear.
     if "email" in session:
-        companias = config.Read("select dbo.Proveedor.ID_COMPANIA,dbo.Proveedor.NOMBRE,dbo.Intermediario.NOMBRE,dbo.Intermediario.AP_MAT,dbo.Intermediario.AP_MAT,dbo.Intermediario.TEL From dbo.Proveedor inner join dbo.Intermediario on dbo.Intermediario.ID_COMPANIA = dbo.Proveedor.ID_COMPANIA")
-        return render_template(
-            "products/manage-company.html",
-            companias=companias
+        companias = config.Read(
+            "SELECT Proveedor.ID_COMPANIA, Proveedor.NOMBRE, Intermediario.NOMBRE, Intermediario.AP_PAT, Intermediario.AP_MAT, Intermediario.TEL,  Intermediario.ID_INTERMEDIARIO FROM dbo.Proveedor INNER JOIN dbo.Intermediario ON Intermediario.ID_COMPANIA = Proveedor.ID_COMPANIA WHERE dbo.Proveedor.ESTATUS = 1 AND dbo.Intermediario.ESTATUS = 1"
         )
+        return render_template("products/manage-company.html", companias=companias)
     else:
-        return redirect(url_for("signin"))
+        return redirect(url_for("auth.signin"))
+
+
+@products.route("/delete_company", methods=["POST"])
+def delete_company():
+    # Verificar si el usuario tiene una sesión activa
+    if "email" in session:
+        # Verificar si el método de solicitud es POST
+        if request.method == "POST":
+            # Obtener el ID del intermediario de los datos del formulario
+            company_id = request.form.get("company_id")
+            print("========================================\nDATOS OBTENIDOS>\n")
+            print(f"company_id - {company_id}")
+            print("========================================")
+            config.CUD(
+                """
+                UPDATE dbo.Intermediario SET ESTATUS = 0 WHERE ID_COMPANIA = {};
+                UPDATE dbo.Proveedor SET ESTATUS = 0 WHERE ID_COMPANIA = {};
+                """.format(
+                    company_id, company_id
+                )
+            )
+            return redirect(url_for("products.managecompany"))
+    # Si el usuario no tiene una sesión activa, redirigirlo a la página de inicio de sesión
+    return redirect(url_for("auth.signin"))
 
 
 @p.route("/welcomeuser")
 def welcomeuser():
-    return render_template("profile/welcome-user.html")
+    if "email" in session:
+        print("========================================\nSession>\n")
+        print(session)
+        return render_template("profile/welcome-user.html")
+    else:
+        return redirect(url_for("auth.signin"))
 
 
 @p.route("/profile")
@@ -248,18 +292,15 @@ def settings():
     return render_template("settings/settings.html")
 
 
-# Sign-Out
-@app.route("/Signout")
-def Signout():
+@app.route("/signout")
+def signout():
     if "email" in session:
-        session.pop(
-            "email", None
-        )  # Eliminar la clave 'email' de la sesión si está presente
+        session.pop("email", None)
         print("CERRANDO SESSION...")
-        return redirect("auth/signin")
+        return redirect(url_for("auth.signin"))
     else:
         print("NO HAY SESSION")
-    return redirect(url_for("auth/signin"))
+    return redirect(url_for("auth.signin"))
 
 
 # Registrar los Blueprints en la App
