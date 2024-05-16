640d59e4b5b1eef6cd2a749ffad14a3eea75ed7f
xEmmanuelCobax <emmanuelcobacuevas@gmail.com>
2024-05-15 19:46:02 -0600

    V2

--- marketMAIN/config.py
+++ marketMAIN/config.py
@@ -7,7 +7,7 @@ def CUD(query):
     try:
         # Conectar a la BD DRIVER={SQL Server};SERVER=NOMBRE DEL SERVIDOR;DATABASE=NOMBRE DE LA BASE DE DATOS;Trusted_Connection=yes; EL TRUSTED ES PARA PODER CONECTAR SIN TENER CONTRASENAS
         connection = pyodbc.connect(
-            "DRIVER={SQL Server};SERVER=PCEMMANUEL;DATABASE=Proyecto;Trusted_Connection=yes;"
+            "DRIVER={SQL Server};SERVER=DESKTOP-ACD3GSV;DATABASE=Proyecto;Trusted_Connection=yes;"
         )
         print("========================================\nConexión exitosa")
         cursor = connection.cursor()
@@ -26,7 +26,7 @@ def Read(query):
     print("Conectando...")
     try:
         connection = pyodbc.connect(
-            "DRIVER={SQL Server};SERVER=PCEMMANUEL;DATABASE=Proyecto;Trusted_Connection=yes;"
+            "DRIVER={SQL Server};SERVER=DESKTOP-ACD3GSV;DATABASE=Proyecto;Trusted_Connection=yes;"
         )
         print("========================================\nConexion exitosa")
         cursor = connection.cursor()
